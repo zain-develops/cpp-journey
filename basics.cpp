ba4869c60565b686ada8d950083fbf9262463b0e
Zain <za4innn@gmail.com>
2026-08-14 18:25:11 +0500

    Going through basics

---
+++ basics.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello World!" << '\n';
+    cout <<"How are you!" << endl;
+
+    //int (Whole numbers)
+    int x = 234;
+    int y = 78;
+    int sum = x + y;
+    int diff = x - y;
+
+    cout <<x <<"\n";
+    cout <<y <<"\n";
+    cout <<sum <<"\n";
+    cout <<diff <<"\n";
+
+    //char (Single Character)
+    char grade = 'A';
+    cout <<grade <<'\n';
+
+
+    //double (decimal numbers)
+    double Gpa = 3.8;
+    double petrolrate = 324;
+    double temperature = 37.2;
+
+    cout <<Gpa <<"\n";
+    cout <<petrolrate <<"\n";
+    cout <<temperature <<"\n";
+
+    //string (Sequence of Characters)
+    string greetings = "Hello! How are you?";
+    string name = "What's your name?";
+
+    cout <<greetings <<"\n";
+    cout <<name <<"\n";
+
+    //Boolean (true or false)
+    bool eligible = false;
+    bool learning = true;
+
+    cout <<eligible <<"\n";
+    cout <<learning <<"\n";
+
+}
+
