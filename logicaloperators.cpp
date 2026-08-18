f3f01e6031de2cbb758f79ba28d2f9eed9b17b18
Zain <za4innn@gmail.com>
2026-08-18 13:32:06 +0500

    Going through logical operators

---
+++ logicaloperators.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+    
+    //demonstration of logical operators "&&" and "||"
+    //&& is an operator used when we want all conditions to be true
+    if( age >= 20 && age <=30 ){
+      cout << "You're eligible for this job vaccancy except internships." <<"\n";
+
+    }
+    
+    //|| is an or operator used when we want atleast one condtion to be true
+    else if( age == 18 || age == 19 ){
+      cout << "You're only eligible for intrenships!" <<"\n"; 
+    }
+
+    else{
+        cout <<"You're ineligible for this job!" <<"\n";
+    }
+
+    //demostration of Not operator "!"
+    bool marks;
+    
+    cout <<"Enter your marks : ";
+    cin >> marks;
+    
+    //By using "!" result was changed from true to false
+    if( !marks < 60 ){
+        cout << "You've sufficient marks for this job!" << endl ;
+    }
+
+    else{
+        cout << "You've insufficient marks for this job!" << endl ;
+    }
+
+    
+    return 0;
+}
