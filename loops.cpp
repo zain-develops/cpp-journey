035cfbada2c77a5c0a869bb9ddbeb400a79066f4
Zain <za4innn@gmail.com>
2026-08-22 16:04:56 +0500

    Hands on lopps

---
+++ loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+   //loops
+   for (int i = 0; i <= 5; i++)
+   {
+    std::cout << "I love donuts." <<std::endl;
+   }
+   
+   for (int i = 0; i <= 10; i++)
+   {
+    std::cout << i <<std::endl;
+   }
+   
+    std::cout << "End!";
+
+
+
+}
