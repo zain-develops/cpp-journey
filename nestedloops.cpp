073bc264310dbf5cc39950b801e5f29523aedc54
Zain <za4innn@gmail.com>
2026-08-24 16:45:27 +0500

    Working on nested loops

--- nestedloops.cpp
+++ nestedloops.cpp
@@ -13,7 +13,30 @@ int main(){
        std::cout <<"\n";
     }
     
+    //This code will print out any character you want into numbers of rows or columns of your choice
+    int rows;
+    int columns;
+    char symbol;
 
+    std::cout <<"Enter number of rows : ";
+    std::cin >> rows;
+
+    std::cout <<"Enter number of columns : ";
+    std::cin >> columns;
+
+    std::cout <<"Enter symbol : ";
+    std::cin >> symbol;
+
+
+    for (int i = 1; i <= rows ; i++)
+    {
+        for (int j = 1; j <= columns ; j++)
+        {
+            std::cout << symbol ;
+        }
+       std::cout <<"\n";
+    }
+    
    
     
 
