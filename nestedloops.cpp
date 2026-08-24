8f28f4f574d5e921c8b2e0e78b6e763b2ee029c4
Zain <za4innn@gmail.com>
2026-08-24 16:35:32 +0500

    Working on nested loops

---
+++ nestedloops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    //nested loops
+    //we print the table of 5 three times using nested loops
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 3; j <= 30; j+=3)
+        {
+            std::cout << j << " ";
+        }
+       std::cout <<"\n";
+    }
+    
+
+   
+    
+
+}
