dab9aeffd1f0165008ef1e2f72573abb3fedf194
Zain <za4innn@gmail.com>
2026-09-02 22:09:28 +0500

    Working with arrays

--- arrays.cpp
+++ arrays.cpp
@@ -11,4 +11,22 @@ int main (){
      std::cout << array[3] << " :)\n";
      std::cout << array[4] << " :)\n";
 
+    //sizeof() operator
+    int num = 12;
+    double dec = 4.34;
+    std::string name = "Bro";
+    bool right = true;
+    char grade = 'B';
+    std::string clubs[] = { "Real madrid" , "Chelsea" , "Bayern Munich" };
+
+    std::cout << sizeof(num) << "Bytes\n";
+    std::cout << sizeof(dec) << "Bytes\n";
+    std::cout << sizeof(name) << "Bytes\n";
+    std::cout << sizeof(right) << "Bytes\n";
+    std::cout << sizeof(grade) << "Bytes\n";
+    std::cout << sizeof(clubs) << "Bytes\n";
+
+
+    return 0;
+
 }
