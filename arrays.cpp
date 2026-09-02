6ab274e77437e5b884612170bd7dc23cc886dbd9
Zain <za4innn@gmail.com>
2026-09-02 21:57:32 +0500

    Working with arrays

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main (){
+
+
+    //arrays
+    int array[] = { 10 , 20 , 30 , 40 , 50 };
+     std::cout << array[0] << " :)\n";
+     std::cout << array[1] << " :)\n";
+     std::cout << array[2] << " :)\n";
+     std::cout << array[3] << " :)\n";
+     std::cout << array[4] << " :)\n";
+
+}
