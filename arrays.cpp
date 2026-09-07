1f7fee4f5a744add4a3fe1ed22d837d09644ab11
Zain <za4innn@gmail.com>
2026-09-07 06:16:07 +0500

    Working with arrays

--- arrays.cpp
+++ arrays.cpp
@@ -125,6 +125,21 @@ int main (){
         std::cout << flavours<<'\n';
 
     }
+
+    //2d arrays(multidimensionla arrays)
+    std::string cars[][3] = {{"Audi" , "BMW" , "Mercedes"},
+                             {"Lamborghini" , "Ferrari" , "Pagani"},
+                             {"Toyota" , "Honda" , "Nissan"}};
+
+    std::cout<< cars[0][0]<< " ";                         
+    std::cout<< cars[0][1]<< " ";                         
+    std::cout<< cars[0][2]<< "\n";                         
+    std::cout<< cars[1][0]<< " ";                         
+    std::cout<< cars[1][1]<< " ";                         
+    std::cout<< cars[1][2]<< "\n";                         
+    std::cout<< cars[2][0]<< " ";                         
+    std::cout<< cars[2][1]<< " ";                         
+    std::cout<< cars[2][2]<< "\n"; 
     
     return 0;
     
